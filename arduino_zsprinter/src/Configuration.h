22ce2f39d1926e1a5fa50eb7c551355a9aeff406
Shohei Aoki <shoaok@gmail.com>
2018-09-28 09:13:28 +0300

    inverted X-axis and Y-axis

--- arduino_zsprinter/src/Configuration.h
+++ arduino_zsprinter/src/Configuration.h
@@ -83,11 +83,11 @@
 // The pullups are needed if you directly connect a mechanical endswitch between the signal and ground pins.
 //If your axes are only moving in one direction, make sure the endstops are connected properly.
 //If your axes move in one direction ONLY when the endstops are triggered, set [XYZ]_ENDSTOP_INVERT to true here:
-//const bool X_ENDSTOP_INVERT = false;
-//const bool Y_ENDSTOP_INVERT = false;
+const bool X_ENDSTOP_INVERT = false;
+const bool Y_ENDSTOP_INVERT = false;
 //const bool Z_ENDSTOP_INVERT = false;
-const bool X_ENDSTOP_INVERT = true;
-const bool Y_ENDSTOP_INVERT = true;
+// const bool X_ENDSTOP_INVERT = true;
+// const bool Y_ENDSTOP_INVERT = true;
 const bool Z_ENDSTOP_INVERT = true;
 
 // This determines the communication speed of the printer
